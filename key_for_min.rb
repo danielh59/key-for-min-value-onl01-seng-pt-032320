edf414fb7aa195e9e969594ebf4ae0986c7ad66f
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-05 17:25:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+new_hash = {"first_one" => 1, "the_second" => 2, "the_third" => 3}
 
 def key_for_min_value(name_hash)
 
