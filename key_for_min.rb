573a5fe8d236da22224cb3a5513c9eacc42fb871
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-05 18:28:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ new_hash = {"first_one" => 1, "the_second" => 2, "the_third" => 3}
 def key_for_min_value(name_hash)
 lowest_key = nil 
 lowest_value = nil 
-hash.each do |key, value|
+name_hash.each do |key, value|
   if lowest_value == nil || value < lowest_value 
     lowest_value = value 
     lowest_key = key 
